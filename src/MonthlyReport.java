fd4f45771e6463e1373a73d474112146a7ec4d85
Roman <romanukyan86@yandex.ru>
2023-08-05 21:55:08 +0300

    Дописаны методы getMonthlyReport(), mostProfitableProduct(), biggestExpense(), getSumIncome(), getSumExpense() в классе MonthlyReport().

--- src/MonthlyReport.java
+++ src/MonthlyReport.java
@@ -2,33 +2,59 @@ import java.util.ArrayList;
 import java.util.HashMap;
 
 public class MonthlyReport {
-    HashMap<String, ArrayList<String>> monthlyReport;
-
+    HashMap<Integer, ArrayList<String>> monthlyReport;
+    String[] months = {"Январь", "Февраль", "Март"};
     Transaction transaction;
-    MonthlyReport(HashMap<String, ArrayList<String>> monthlyReport){
+    MonthlyReport(HashMap<Integer, ArrayList<String>> monthlyReport){
         this.monthlyReport = monthlyReport;
-    }
-
-
-    void printMonthlyReport() { // По сохранённым данным вывести в консоль имеющуюся информацию
-
-                //1 - название месяца;
-        mostProfitableProduct();       //2 - самый прибыльный товар, название товара и сумму;
-        biggestExpense();        //3 - самую большую трату, название товара и сумму.
-
-
 
     }
 
-    void mostProfitableProduct(){
 
-        // самый прибыльный товар, название товара и сумму;
-
+    void getMonthlyReport() { // По сохранённым данным вывод в консоль имеющейся информации
+        System.out.println("Информация обо всех месячных отчётах.");
+        for (int i = 1; i < 4; i++) {
+            System.out.println(months[i]);
+            mostProfitableProduct(monthlyReport.get(i));
+            biggestExpense(monthlyReport.get(i));
+            System.out.println();
+        }
     }
 
-    void biggestExpense(){
+    void mostProfitableProduct(ArrayList<String> lines){ // Самый прибыльный товар, название товара и сумму;
+        String nameOfProfitableProduct = null;
+        int sumProfit = 0;
+        for (int i = 1; i < lines.size(); i++) {
+            String line = lines.get(i);
+            transaction = new Transaction(line);
+            if (!transaction.isExpense){
+                int profit = transaction.unitPrice * transaction.quantity;
+                if (profit > sumProfit){
+                    nameOfProfitableProduct = transaction.name;
+                    sumProfit = profit;
+                }
+            }
+        }
+        System.out.println("Самый прибыльный товар: " + nameOfProfitableProduct + " " + sumProfit);
+    }
 
-        // самую большую трату, название товара и сумму.
+    void biggestExpense(ArrayList<String> lines){ // Самая большая трата, название товара и сумму.
+        String nameOfBiggestExpense = null;
+        int sumExpense = 0;
+        for (int i = 1; i < lines.size(); i++) {
+            String line = lines.get(i);
+            transaction = new Transaction(line);
+            if (transaction.isExpense){
+                int expense = transaction.unitPrice * transaction.quantity;
+                if (expense > sumExpense){
+                    nameOfBiggestExpense = transaction.name;
+                    sumExpense = expense;
+                }
+            }
+        }
+        System.out.println("Самый прибыльный товар: " + nameOfBiggestExpense + " " + sumExpense);
+
+
 
     }
 
@@ -43,5 +69,15 @@ public class MonthlyReport {
         }
         return sumIncome;
     }
-
+    int getSumExpense(ArrayList<String> lines) {
+        int sumExpense = 0;
+        for (int i = 1; i < lines.size(); i++) {
+            String line = lines.get(i);
+            transaction = new Transaction(line);
+            if (transaction.isExpense) {
+                sumExpense += transaction.quantity * transaction.unitPrice;
+            }
+        }
+        return sumExpense;
+    }
 }
