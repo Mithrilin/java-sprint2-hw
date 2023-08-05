3529c8a898eefb0b02c6321f2258668f007a9657
Roman <romanukyan86@yandex.ru>
2023-08-05 22:38:49 +0300

    Исправлена ошибка из-за которой isExpense всегда присваивалось false.

--- src/Transaction.java
+++ src/Transaction.java
@@ -6,12 +6,8 @@ public class Transaction {
     Transaction(String line){
         String[] lineContents = line.split(",");
         this.name = lineContents[0];
-        this.isExpense = lineContents[1].equals("true");
         this.quantity = Integer.parseInt(lineContents[2]);
         this.unitPrice = Integer.parseInt(lineContents[3]);
-
+        this.isExpense = lineContents[1].equals("TRUE");
     }
-
-
-
 }
